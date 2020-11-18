ac7f213b8b4daa5315ae3f44d9cbdeba9bdee4ba
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 19:23:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  Hash.new(my_hash[key] = value)
+  Hash.new([key] = value)
 end
 
 def read_from_hash(hash, key)
